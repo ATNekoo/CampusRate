38a11a42726fad9b3e2d05002ba7d5848bf5efdd
ATNekoo <nekotockson@gmail.com>
2026-09-23 23:13:30 -0400

    Squelettes des modules créés

--- README.md
+++
@@ -1,2 +0,0 @@
-# CampusRate
-Conception d’une API REST professionnelle || 420-514 Collecte et interprétation des données || Techniques de l’informatique Automne 2026
---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { StorageModule } from './storage/storage.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { PlacesModule } from './places/places.module';
+
+@Module({
+  imports: [StorageModule, ReviewsModule, PlacesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
